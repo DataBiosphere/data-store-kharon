d323dbcc61994c9e300adddf556196d8ed2d896c
Brian Hannafious <bhannafi@ucsc.edu>
2018-10-25 15:35:53 -0700

    Add GCP service account with deletion authorization (#4)

---
+++ infra/gcp_service_account/main.tf
@@ -0,0 +1,21 @@
+data "google_project" "project" {}
+
+resource "google_service_account" "dss" {
+  display_name = "${var.DDS_GCP_SERVICE_ACCOUNT_NAME}"
+  account_id = "${var.DDS_GCP_SERVICE_ACCOUNT_NAME}"
+}
+
+# Useful command to discover role names (Guessing based on console titles is difficult):
+# `gcloud iam list-grantable-roles //cloudresourcemanager.googleapis.com/projects/{project-id}`
+
+resource "google_project_iam_member" "storageobjectadmin" {
+  project = "${data.google_project.project.project_id}"
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.dss.email}"
+}
+
+resource "google_project_iam_member" "viewer" {
+  project = "${data.google_project.project.project_id}"
+  role    = "roles/viewer"
+  member  = "serviceAccount:${google_service_account.dds.email}"
+}
