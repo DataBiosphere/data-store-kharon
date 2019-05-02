c0ed5760e985cf630c52e2ca238bfd28397783bc
DailyDreaming <lblauvel@ucsc.edu>
2019-05-02 14:03:04 -0700

    Deindex collections upon delete.

--- dds/__init__.py
+++ dds/__init__.py
@@ -2,13 +2,16 @@ import os
 import boto3
 import logging
 import functools
+import json
 from google.cloud.storage import Client
 from cloud_blobstore import BlobNotFoundError
 from cloud_blobstore.s3 import S3BlobStore
 from cloud_blobstore.gs import GSBlobStore
 from elasticsearch import Elasticsearch, RequestsHttpConnection
-from elasticsearch import TransportError
 from requests_aws4auth import AWS4Auth
+
+dynamodb_client = boto3.client('dynamodb')
+collection_db_table = f"dss-collections-db-{os.environ['DDS_DEPLOYMENT_STAGE']}"
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -17,6 +20,16 @@ BUCKETS = dict(
     aws=os.environ['DSS_S3_BUCKET'],
     gcp=os.environ['DSS_GS_BUCKET'],
 )
+
+
+def _format_dynamodb_item(hash_key, sort_key, value):
+    item = {'hash_key': {'S': hash_key}}
+    if value:
+        item['body'] = {'S': value}
+    if sort_key:
+        item['sort_key'] = {'S': sort_key}
+    return item
+
 
 @functools.lru_cache()
 def get_es_client():
@@ -38,6 +51,7 @@ def get_es_client():
     )
     return es_client
 
+
 @functools.lru_cache()
 def get_handle(replica):
     if "aws" == replica:
@@ -52,6 +66,7 @@ def get_handle(replica):
         logger.error(msg)
         raise Exception(msg)
 
+
 @functools.lru_cache()
 def get_whitelist():
     ddb_client = boto3.client('dynamodb')
@@ -59,7 +74,8 @@ def get_whitelist():
     keys = {item['key']['S'] for item in items}
     return keys
 
-def deindex(key):
+
+def deindex_bundle(key):
     if key.startswith("bundles/"):
         es_client = get_es_client()
         fqid = key.split("/")[1]
@@ -67,18 +83,32 @@ def deindex(key):
             index="_all",
             body= {"query":{"terms":{"_id":[fqid]}}}
         )
-        logger.info(f"Removed {fqid} from all ES indices")
+        logger.info(f"Removed bundle {fqid} from all ES indices.")
+
+
+def deindex_collection(key, handle, bucket):
+    if key.startswith("collections/"):
+        fqid = key.split("/")[1]
+        collection = json.loads(handle.get(bucket, key))
+        query = {'TableName': collection_db_table,
+                 'Key': _format_dynamodb_item(hash_key=collection['owner'],
+                                     sort_key=key, value=None)}
+        dynamodb_client.delete_item(**query)
+        logger.info(f"Removed collection {fqid} from the dynamoDB index.")
+
 
 def _delete(replica, key):
     try:
         handle = get_handle(replica)
         bucket = BUCKETS[replica]
+        deindex_collection(key, handle, bucket)  # collection must be opened before deletion to determine owner key
         handle.get_size(bucket, key)
         handle.delete(bucket, key)
         logger.info(f"Deleted {key} from bucket:{bucket} replica:{replica}")
     except BlobNotFoundError:
-        logger.info(f"Object not fonud: {key} bucket:{bucket} replica:{replica}")
+        logger.info(f"Object not found: {key} bucket:{bucket} replica:{replica}")
         pass
+
 
 def delete(key):
     if key in get_whitelist():
@@ -86,4 +116,4 @@ def delete(key):
     else:
         for replica in BUCKETS:
             _delete(replica, key)
-        deindex(key)
+        deindex_bundle(key)
