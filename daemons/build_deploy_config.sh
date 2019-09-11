338f6076b8e0756025d8173c92cf1b07fa23a0fa
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2019-09-11 15:56:47 -0700

    Pull in correct DSS ES endpoint from SSM (#50)

--- daemons/build_deploy_config.sh
+++ daemons/build_deploy_config.sh
@@ -19,16 +19,14 @@ config_json="$(dirname $0)/${daemon_name}/.chalice/config.json"
 export iam_role_arn=$(cd $daemon_name ; terraform output role-arn)
 cat "$config_json" | jq .manage_iam_role=false | jq .iam_role_arn=env.iam_role_arn | sponge "$config_json"
 
-data_store_env=$(aws ssm get-parameter --name /dcp/dss/${DDS_DEPLOYMENT_STAGE}/environment | jq .Parameter.Value)
-data_store_env_json=$(echo $data_store_env | python -c "import sys, json; print(json.load(sys.stdin))")
-
-export DSS_ES_ENDPOINT=$(aws ssm get-parameters --names "/dcp/dss/dev/environment" | jq -r '.Parameters[].Value' | jq -r '.DSS_ES_ENDPOINT')
+data_store_env=$(aws ssm get-parameter --name /dcp/dss/${DDS_DEPLOYMENT_STAGE}/environment | jq -r .Parameter.Value | jq .)
+export DSS_ES_ENDPOINT=$(echo $data_store_env | jq -r .DSS_ES_ENDPOINT)
 
 for var in $EXPORT_ENV_VARS_TO_LAMBDA DSS_ES_ENDPOINT; do
     cat "$config_json" | jq .stages.$stage.environment_variables.$var=env.$var | sponge "$config_json"
 done
 
 for var in DSS_S3_BUCKET DSS_GS_BUCKET; do
-    val=$(echo $data_store_env_json | jq .$var)
+    val=$(echo $data_store_env | jq .$var)
     cat "$config_json" | jq .stages.$stage.environment_variables.$var="$val" | sponge "$config_json"
 done
